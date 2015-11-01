a1217a79ac36583785d41ded19aafd32d8a7ebd8
bveronese <bruno_veronese@hotmail.com>
2015-11-01 03:40:42 -0200

    Threads implementation started

--- Cipher/src/app/Program.java
+++ Cipher/src/app/Program.java
@@ -2,11 +2,14 @@ package app;
 
 import java.io.File;
 
+import app.io.Reader;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		String filename;
+		// List<String> toCipher;
 		
 		if (args.length < 1)
 			System.exit(1);
@@ -22,6 +25,16 @@ public class Program {
 		if(file.isDirectory())
 			System.exit(1);
 
+		Thread reader = new Thread(new Reader(file));
+		reader.start();
+		
+		
+		try {
+			reader.join();
+		} catch (InterruptedException e) {
+			System.err.println(e.getMessage());
+		}
+		
 	}
 
 }
--- Cipher/src/app/io/Reader.java
+++ Cipher/src/app/io/Reader.java
@@ -1,5 +1,33 @@
 package app.io;
 
-public class Reader {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader implements Runnable {
+
+	private File file;
+	private FileReader fr;
+	
+	public Reader(File file) {
+		this.file = file;
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+			fr = new FileReader(file);
+		
+			char[] a = new char[50];
+			fr.read(a);
+			for (char c : a) {
+				System.out.println(c);
+			}
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+		
+	}
 
 }
