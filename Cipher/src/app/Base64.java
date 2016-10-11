ba784da02260bdaba5c9aa3a8230211084f2f532
veronese <bruno_veronese@hotmail.com>
2016-10-10 21:55:00 -0300

    Lendo arquivos

--- Cipher/src/app/Base64.java
+++ Cipher/src/app/Base64.java
@@ -1,61 +1,106 @@
 package app;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class Base64 {
 
-	 private static final String CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+	private static String BASE64CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-    private static byte[] base64Decode(String input)    {
-        if (input.length() % 4 != 0)    {
-            throw new IllegalArgumentException("Invalid base64 input");
-        }
-        byte decoded[] = new byte[((input.length() * 3) / 4) - (input.indexOf('=') > 0 ? (input.length() - input.indexOf('=')) : 0)];
-        char[] inChars = input.toCharArray();
-        int j = 0;
-        int b[] = new int[4];
-        for (int i = 0; i < inChars.length; i += 4)     {
-            // This could be made faster (but more complicated) by precomputing these index locations.
-            b[0] = CODES.indexOf(inChars[i]);
-            b[1] = CODES.indexOf(inChars[i + 1]);
-            b[2] = CODES.indexOf(inChars[i + 2]);
-            b[3] = CODES.indexOf(inChars[i + 3]);
-            decoded[j++] = (byte) ((b[0] << 2) | (b[1] >> 4));
-            if (b[2] < 64)      {
-                decoded[j++] = (byte) ((b[1] << 4) | (b[2] >> 2));
-                if (b[3] < 64)  {
-                    decoded[j++] = (byte) ((b[2] << 6) | b[3]);
-                }
+    public static String codificar(byte[] arqBytes) {
+    	
+    	System.out.println( "Iniciando codificação" );
+    	
+        StringBuilder saida = new StringBuilder(( arqBytes.length * 4 ) / 3);
+        int ponteiro;
+        for ( int i = 0; i < arqBytes.length; i += 3 ) {
+            ponteiro = ( arqBytes[ i ] & 0xFC ) >> 2;
+            saida.append( BASE64CHARS.charAt( ponteiro ) );
+            ponteiro = ( arqBytes[ i ] & 0x03 ) << 4;
+            if ( i + 1 >= arqBytes.length ) {
+            	saida.append( BASE64CHARS.charAt( ponteiro ) ).append( "==" );
+            	continue;
             }
-        }
+            
+            ponteiro |= ( arqBytes[ i + 1 ] & 0xF0 ) >> 4;
+            saida.append( BASE64CHARS.charAt( ponteiro ) );
+            ponteiro = ( arqBytes[ i + 1 ] & 0x0F ) << 2;
 
-        return decoded;
-    }
-
-    private static String base64Encode(byte[] in)       {
-        StringBuilder out = new StringBuilder((in.length * 4) / 3);
-        int b;
-        for (int i = 0; i < in.length; i += 3)  {
-            b = (in[i] & 0xFC) >> 2;
-            out.append(CODES.charAt(b));
-            b = (in[i] & 0x03) << 4;
-            if (i + 1 < in.length)      {
-                b |= (in[i + 1] & 0xF0) >> 4;
-                out.append(CODES.charAt(b));
-                b = (in[i + 1] & 0x0F) << 2;
-                if (i + 2 < in.length)  {
-                    b |= (in[i + 2] & 0xC0) >> 6;
-                    out.append(CODES.charAt(b));
-                    b = in[i + 2] & 0x3F;
-                    out.append(CODES.charAt(b));
-                } else  {
-                    out.append(CODES.charAt(b));
-                    out.append('=');
-                }
-            } else      {
-                out.append(CODES.charAt(b));
-                out.append("==");
+            if ( i + 2 >= arqBytes.length ) {
+            	saida.append( BASE64CHARS.charAt( ponteiro ) ).append( '=' );
+            	continue;
             }
+            
+            ponteiro |= ( arqBytes[ i + 2 ] & 0xC0 ) >> 6;
+            saida.append( BASE64CHARS.charAt( ponteiro ) );
+            ponteiro = arqBytes[ i + 2 ] & 0x3F;
+            saida.append( BASE64CHARS.charAt( ponteiro ) );
+        }
+        
+        System.out.println( "Encerrando codificação: " + saida );
+        return saida.toString();
+    }
+    
+    private static String codificarArquivo( File arq ) throws Exception {
+    	
+    	FileInputStream fis = null;
+
+        byte[] bFile = new byte[ ( int ) arq.length() ];
+
+	    fis = new FileInputStream( arq );
+	    fis.read( bFile );
+	    fis.close();
+
+    	return codificar( bFile );
+    }
+    
+    public static byte[] decodificar( String textoEncodado )    {
+    	   
+        if ( textoEncodado.length() % 4 != 0 ) return null;
+
+        System.out.println( "Iniciando decodificação: " + textoEncodado );
+        
+        int menosTamanho = 0, y = 0;
+        if( textoEncodado.indexOf( '=' ) > 0 ) {
+        	menosTamanho = ( textoEncodado.length() - textoEncodado.indexOf( '=' ) );
+        }
+        
+        byte bytesDecodificados[] = new byte[ ( ( textoEncodado.length() * 3 ) / 4 ) - menosTamanho ];
+        char[] txtEncArr = textoEncodado.toCharArray();
+        int ponteiro[] = new int[ 4 ];
+        
+        for ( int i = 0; i < txtEncArr.length; i += 4 )	{
+            ponteiro[ 0 ] = BASE64CHARS.indexOf( txtEncArr[ i ] );
+            ponteiro[ 1 ] = BASE64CHARS.indexOf( txtEncArr[ i + 1 ] );
+            ponteiro[ 2 ] = BASE64CHARS.indexOf( txtEncArr[ i + 2 ] );
+            ponteiro[ 3 ] = BASE64CHARS.indexOf( txtEncArr[ i + 3 ] );
+            
+            bytesDecodificados[ y++ ] = ( byte ) ( ( ponteiro[ 0 ] << 2 ) | ( ponteiro[ 1 ] >> 4 ) );
+            
+            if ( ponteiro[ 2 ] >= 64 ) continue;
+            
+            bytesDecodificados[ y++ ] = ( byte ) ( ( ponteiro[ 1 ] << 4) | ( ponteiro[ 2 ] >> 2 ) );
+            
+            if ( ponteiro[ 3 ] >= 64 ) continue;
+
+            bytesDecodificados[ y++ ] = ( byte ) ( ( ponteiro[ 2 ] << 6 ) | ponteiro[ 3 ] );
         }
 
-        return out.toString();
+        System.out.println( "Encerrando decodificação" );
+        return bytesDecodificados;
     }
+    
+    public static void main( String[] args ) throws Exception {
+		
+    	File arq1 = new File( "D:\\Testando.txt" );
+    	String stringona = codificarArquivo( arq1 );
+
+    	File arq2 = new File( "D:\\TestandoDecodado.txt" );
+    	FileOutputStream fos = new FileOutputStream( arq2 );
+    	fos.write( decodificar( stringona ) );
+    	fos.close();
+    	
+    	System.out.println(arq1.compareTo(arq2));
+	}
 }
